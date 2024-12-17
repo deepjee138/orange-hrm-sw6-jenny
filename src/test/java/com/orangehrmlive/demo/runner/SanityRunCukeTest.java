8f44c800ca2c7419f8e29cc886520223cc58c256
deepjee138 <deepjenny11@gmail.com>
2024-12-17 11:35:53 +0000

    done

---
+++ src/test/java/com/orangehrmlive/demo/runner/SanityRunCukeTest.java
@@ -0,0 +1,25 @@
+package com.orangehrmlive.demo.runner;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+/**
+ * Created by jenny
+ *
+ */
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/orangehrmlive/demo/steps",
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "json:target/cucumber-reports/cucumber.json"
+        },
+        tags = "@sanity"
+
+
+)
+public class SanityRunCukeTest extends AbstractTestNGCucumberTests {
+
+
+}
